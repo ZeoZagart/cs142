719e723500ec2db50d62796f007418c5dc564702
ZeoZagart <abhashya@flock.com>
2020-07-27 00:09:30 +0530

    fix Drawer

--- project5react/components/topBar/TopBar.jsx
+++ project5react/components/topBar/TopBar.jsx
@@ -1,43 +1,62 @@
 import React from "react";
-import clsx from 'clsx';
-import { AppBar, Toolbar, Typography, Divider, IconButton, Drawer, CssBaseline } from "@material-ui/core";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import clsx from "clsx";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Divider,
+  IconButton,
+  Drawer,
+  CssBaseline,
+} from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 
 class TopBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       open: false,
-    }
-    this.handleDrawerOpen = this.handleDrawerOpen.bind(this)
-    this.handleDrawerClose = this.handleDrawerClose.bind(this)
+    };
+    this.handleDrawerOpen = this.handleDrawerOpen.bind(this);
+    this.handleDrawerClose = this.handleDrawerClose.bind(this);
   }
 
   handleDrawerOpen(event) {
-    this.setState ({
+    this.setState({
       open: true,
-    })
-  };
+    });
+  }
 
   handleDrawerClose(event) {
-    this.setState ({
+    this.setState({
       open: false,
-    })
-  };
-
+    });
+  }
 
   render() {
-    const classes = this.useStyles();
+    const { classes } = this.props;
+    open = this.state.open;
     return (
       <div className={classes.root}>
-        <CssBaseline/>
-        <AppBar position="fixed" className={clsx(classes.appBar, {[classes.appBarShift]: this.state.open,})}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
           <Toolbar>
-            <IconButton color="inherit" aria-label="open drawer" onClick={this.handleDrawerOpen} 
-              edge="start" className={clsx(classes.menuButton, {[classes.hide]: this.state.open,})}>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={this.handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, {
+                [classes.hide]: open,
+              })}
+            >
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" noWrap>
@@ -49,13 +68,13 @@ class TopBar extends React.Component {
           variant="permanent"
           className={clsx(classes.drawer, {
             [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open
+            [classes.drawerClose]: !open,
           })}
           classes={{
             paper: clsx({
               [classes.drawerOpen]: open,
-              [classes.drawerClose]: !open
-            })
+              [classes.drawerClose]: !open,
+            }),
           }}
         >
           <div className={classes.toolbar}>
@@ -63,78 +82,75 @@ class TopBar extends React.Component {
               <ChevronLeftIcon />
             </IconButton>
           </div>
-          <Divider/>
-
+          <Divider />
         </Drawer>
       </div>
     );
   }
-
-  getTheme(){ return () => useTheme()};
-
-  useStyles() {
-    return makeStyles((theme) => ({
-      root: {
-        display: 'flex',
-      },
-      appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-      },
-      appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      },
-      menuButton: {
-        marginRight: 36,
-      },
-      hide: {
-        display: 'none',
-      },
-      drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-      },
-      drawerOpen: {
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      },
-      drawerClose: {
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-        overflowX: 'hidden',
-        width: theme.spacing(7) + 1,
-        [theme.breakpoints.up('sm')]: {
-          width: theme.spacing(9) + 1,
-        },
-      },
-      toolbar: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-      },
-      content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-      },
-    }));
-  }
 }
 
-export default TopBar;
+const drawerWidth = 240;
+
+const styles = (theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+});
+
+export default withStyles(styles)(TopBar);
